83d29c8def9fdccc302d8adbd3f9a2611469fc37
Andrei <marciucandrei0@gmail.com>
2018-01-17 06:50:53 -0800

    last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string;
+  colors: string[];
+  barcode: string[];
+
+  constructor() {
+    this.title = 'Retro Barcode Generator';
+    this.colors = ['#8A2BE2','#DC143C','#008B8B','#00BFFF','#F8F8FF','#FF69B4','#00FF00','#FF00FF','#FF4500','#00FF7F','#FFFF00','#EE82EE','#663399','#0000CD','LimeGreen '];
+    this.barcode = [];
+    for (var x = 0; x < 10; x++) {
+      this.barcode[x] = this.colors[Math.floor(Math.random()*this.colors.length)];
+    }
+  }
 }
